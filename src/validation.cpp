efcdf3c3b8ac8895d5be0c0a271fdc6301c8bcfa
Sotashi <47956471+Sotashi@users.noreply.github.com>
2019-02-25 02:28:52 +0100

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1151,14 +1151,11 @@ bool ReadRawBlockFromDisk(std::vector<uint8_t>& block, const CBlockIndex* pindex
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
-
     CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
+    
+    // Subsidy  won't be cut in half every 210,000. 
+    // Satoshi's biggest mistake is corrected here.
+
     return nSubsidy;
 }
 
